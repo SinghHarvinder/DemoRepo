f3b978bb33c9566fd7d39ba4a37921d44c418571
DELL@DESKTOP-7QFNEJ0 <harvindersaini2402@gmail.com>
2018-09-24 00:48:33 +0530

    modify

--- src/com/mavendemo/scripts/TestThree.java
+++ src/com/mavendemo/scripts/TestThree.java
@@ -1,5 +1,17 @@
 package com.mavendemo.scripts;
 
+import org.testng.annotations.Test;
+
 public class TestThree {
 
+	
+	@Test
+	public void testOne()
+	{
+		System.out.println("testOne method");
+		
+	}
+	
+	
+	
 }
