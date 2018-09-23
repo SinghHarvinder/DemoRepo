02e95dce285ec4d8a07abb768ae582576d95acc0
DELL@DESKTOP-7QFNEJ0 <harvindersaini2402@gmail.com>
2018-09-23 20:12:01 +0530

    add new file

---
+++ src/com/mavendemo/scripts/TestOne.java
@@ -0,0 +1,14 @@
+package com.mavendemo.scripts;
+
+import org.testng.annotations.Test;
+
+public class TestOne 
+{
+	
+	@Test
+	public void testMethod1()
+	{
+		System.out.println("TestOne Class method testMethod1");
+	}
+
+}
