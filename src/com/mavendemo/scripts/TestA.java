759dc59e835417369761e3bcaad43d3dfbb5cc30
DELL@DESKTOP-7QFNEJ0 <harvindersaini2402@gmail.com>
2018-09-23 18:08:17 +0530

    add TestA.java file

---
+++ src/com/mavendemo/scripts/TestA.java
@@ -0,0 +1,13 @@
+package com.mavendemo.scripts;
+
+import org.testng.annotations.Test;
+
+public class TestA 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("Test1 method running");
+		}
+
+}
