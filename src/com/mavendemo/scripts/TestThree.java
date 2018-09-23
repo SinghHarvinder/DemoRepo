791091c3b81785af3590e1fc265ff315514c5638
DELL@DESKTOP-7QFNEJ0 <harvindersaini2402@gmail.com>
2018-09-24 01:24:27 +0530

    Revert "modify"
    This reverts commit f3b978bb33c9566fd7d39ba4a37921d44c418571.

--- src/com/mavendemo/scripts/TestThree.java
+++ src/com/mavendemo/scripts/TestThree.java
@@ -1,17 +1,5 @@
 package com.mavendemo.scripts;
 
-import org.testng.annotations.Test;
-
 public class TestThree {
 
-	
-	@Test
-	public void testOne()
-	{
-		System.out.println("testOne method");
-		
-	}
-	
-	
-	
 }
